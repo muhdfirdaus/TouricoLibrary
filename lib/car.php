1a4b488244a101c916a26c7723c012f8b0fb9fbe
khairulghazali <khairul.ghazali@misatravel.com>
2015-10-15 14:42:09 +0800

    add new method

--- lib/car.php
+++ lib/car.php
@@ -1,5 +1,6 @@
 <?php 
 
+require '../vendor/autoload.php';
 class Settings {
 public $username;
 public $testEnvironment;
@@ -99,6 +100,188 @@ class Car{
   return $SearchParameters.$CarResults.$Car.$RouteStations.$CarStation;
    
  }
+ 
+ function SearchCarsByAirportCode($params){
+ 	$client = new soapClient($this->baseUrl);
+ 	$result = $client -> SearchCarsByAirportCode($this->convertParamsIntoTouricoParams($params));
+ 	return $this->returnSearchCarsByAirportCode($result);
+ }
+ 
+ function returnSearchCarsByAirportCode($touricoResult){
+ 		
+ 	$link = $touricoResult->envelop;
+ 		
+ 	$d = 'date';
+ 	$SearchParameters = [
+ 				
+ 			'id'   => $link->SearchParameters->id,
+ 			'date' => $link->SearchParameters->$d,
+ 			'hour' => $link->SearchParameters->hour
+ 	];
+ 		
+ 	$CarResults = [
+ 
+ 			'Car' => $link->CarResults->Car
+ 	];
+ 		
+ 	$SearchCarInfo = [
+ 				
+ 			'carCompanyId' => $link->SearchCarInfo->carCompanyId,
+ 			'carCompanyName' => $link->SearchCarInfo->carCompanyName,
+ 			'carName' => $link->SearchCarInfo->carName,
+ 			'carID' => $link->SearchCarInfo->carID,
+ 			'adapterCode' => $link->SearchCarInfo->adapterCode,
+ 			'carThumb' => $link->SearchCarInfo->carThumb,
+ 			'class' => $link->SearchCarInfo->class,
+ 			'transmission' => $link->SearchCarInfo->transmission,
+ 			'ac' => $link->SearchCarInfo->ac,
+ 			'type' => $link->SearchCarInfo->type,
+ 			'maxPassenger' => $link->SearchCarInfo->maxPassenger,
+ 			'doors' => $link->SearchCarInfo->doors,
+ 			'luggageLarge' => $link->SearchCarInfo->luggageLarge,
+ 			'luggageSmall' => $link->SearchCarInfo->luggageSmall,
+ 			'currency' => $link->SearchCarInfo->currency,
+ 			'status' => $link->SearchCarInfo->status,
+ 			'productId' => $link->SearchCarInfo->productId,
+ 			'minAvgPrice' => $link->SearchCarInfo->minAvgPrice,
+ 			'minPrice' => $link->SearchCarInfo->minPrice
+ 	];
+ 		
+ 	$RouteStations = [
+ 				
+ 			'Stations' => $link->RouteStations->Stations
+ 	];
+ 		
+ 	$PickUpStation = [
+ 				
+ 			'id' => $link->PickUpStation->id,
+ 			'name' => $link->PickUpStation->name,
+ 			'address' => $link->PickUpStation->address,
+ 			'phone' => $link->PickUpStation->phone,
+ 			'operationHoursDropOff' => $link->PickUpStation->operationHoursDropOff
+ 	];
+ 		
+ 	$DropOffStation = [
+ 
+ 			'id' => $link->DropOffStation->id,
+ 			'name' => $link->DropOffStation->name,
+ 			'address' => $link->DropOffStation->address,
+ 			'phone' => $link->DropOffStation->phone,
+ 			'operationHoursDropOff' => $link->DropOffStation->operationHoursDropOff
+ 	];
+ 		
+ 	$ProgramList = [
+ 				
+ 			'minPrice' => $link->ProgramList->minPrice
+ 	];
+ 		
+ 	$CarProgram = [
+ 				
+ 			'name' => $link->CarProgram->name,
+ 			'id' => $link->CarProgram->id,
+ 			'price' => $link->CarProgram->price,
+ 			'currency' => $link->CarProgram->currency
+ 	];
+ 		
+ 	$Component = [
+ 				
+ 			'name' => $link->Component->name,
+ 			'id' => $link->Component->id,
+ 			'desc' => $link->Component->desc
+ 	];
+ 		
+ 	$MandatoryFeesDueAtPickup = [
+ 				
+ 			'price' => $link->MandatoryFeesDueAtPickup->price,
+ 			'currency' => $link->MandatoryFeesDueAtPickup->currency
+ 	];
+ 		
+ 	return $SearchParameters.$CarResults.$SearchCarInfo.$RouteStations.$PickUpStation.$DropOffStation.$ProgramList.$CarProgram.$Component.$MandatoryFeesDueAtPickup;
+ 		
+ }
+ 
+ function SelectStations($params){
+ 	$client = new soapClient($this->baseUrl);
+ 	$result = $client -> SelectStations($this->convertParamsIntoTouricoParams($params));
+ 	return $this->returnSelectStations($result);
+ }
+ 
+ function returnSelectStations($touricoResult){
+ 		
+ 	$link = $touricoResult->envelop;
+ 		
+ 	$ProgramList = [
+ 				
+ 			'carProgram' => $link->ProgramList->carProgram,
+ 			'ProgramIncludes' => $link->ProgramList->ProgramIncludes,
+ 			'Component' => $link->ProgramList->Component
+ 	];
+ 		
+ 	$ncp = 'name (CarProgram)';
+ 	$idc = 'id (CarProgram)';
+ 	$pcp = 'price (CarProgram)';
+ 	$ccp = 'currency (CarProgram)';
+ 	$CarProgram = [
+ 				
+ 			'name (CarProgram)' => $link->CarProgram->$ncp,
+ 			'id (CarProgram)' => $link->CarProgram->$idc,
+ 			'price (CarProgram)' => $link->CarProgram->$pcp,
+ 			'currency (CarProgram)' => $link->CarProgram->$ccp
+ 	];
+ 		
+ 	$ProgramIncludes = [
+ 				
+ 			'Component' => $link->ProgramIncludes->Component
+ 	];
+ 		
+ 	$na = 'name (Component)';
+ 	$ia = 'id (Component)';
+ 	$dc = 'desc (Component)';
+ 	$Component = [
+ 				
+ 			'name (Component)' => $link->Component->$na,
+ 			'id (Component)' => $link->Component->$ia,
+ 			'desc (Component)' => $link->Component->$dc
+ 				
+ 	];
+ 		
+ 	$MandatoryFeesDueAtPickup = [
+ 				
+ 			'price' => $link->MandatoryFeesDueAtPickup->price,
+ 			'currency' => $link->MandatoryFeesDueAtPickup->currency
+ 	];
+ 		
+ 	return $ProgramList.$CarProgram.$ProgramIncludes.$Component.$MandatoryFeesDueAtPickup;
+ }
+ 
+ function GetRulesAndRestrictions($params){
+ 	$client = new soapClient($this->baseUrl);
+ 	$result = $client -> GetRulesAndRestrictions($this->convertParamsIntoTouricoParams($params));
+ 	return $this->returnGetRulesAndRestrictions($result);
+ }
+ 
+ function returnGetRulesAndRestrictions($touricoResult){
+ 		
+ 	$link = $touricoResult->envelop;
+ 		
+ 	$Rules = [
+ 				
+ 			'carCompanyID' => $link->Rules->carCompanyID,
+ 			'carCompanyName' => $link->Rules->carCompanyName,
+ 			'carCompanyLogoUrl' => $link->Rules->carCompanyLogoUrl
+ 	];
+ 		
+ 	$idr = 'id (Rule)';
+ 	$Rule = [
+ 				
+ 			'id (Rule)' => $link->Rule->$idr,
+ 			'title' => $link->Rule->title
+ 	];
+ 		
+ 	return $Rules.$Rule;
+ 		
+ }
+ 
 }
   
  ?>
